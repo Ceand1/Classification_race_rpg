684f38a66907953a2ecd3daa41694caa553fae24
Ceand1 <78984017+Ceand1@users.noreply.github.com>
2022-12-06 00:54:26 -0300

    Update test.py

--- test.py
+++ test.py
@@ -15,16 +15,13 @@ from keras import applications
 from keras.utils.np_utils import to_categorical  
 import matplotlib.pyplot as plt 
 import matplotlib.image as mpimg
-#matplotlib inline
 import math  
 import datetime
 import time
 from keras.models import load_model
-from keras.applications.vgg16 import VGG16
 
 vgg16 = applications.VGG16(include_top=False, weights='imagenet') 
 model = load_model('bottleneck_fc_model.h5')
-#model = VGG16(weights = 'model_saved.h5')
 
 def read_image(file_path):
     print("[INFO] loading and preprocessing image...")  
@@ -43,18 +40,6 @@ def test_single_image(path):
     preds = model.predict(bt_prediction)
     for idx, race, x in zip(range(0,6), races , preds[0]):
         print("ID: {}, Label: {} {}%".format(idx, race, round(x*100,2) ))
-    #print('Final Decision:')
-    #time.sleep(.5)
-    #for x in range(3):
-    #    print('.'*(x+1))
-    #    time.sleep(.2)
-    #class_predicted = model.predict_classes(bt_prediction)
-    #class_predicted =model.predict(bt_prediction)
-    #classes_x=np.argmax(class_predicted,axis=1)
-    #class_dictionary = generator_top.class_indices  
-    #inv_map = {v: k for k, v in class_dictionary.items()}  
-    #print("ID: {}, Label: {}".format(classes_x[0], inv_map[classes_x[0]]))  
-    #return load_img(path)
 
 
 path = 'database/test/tieflings/tumblr_bac1a7cf029c0a449c2a500034e908f8_a7a74da3_500.jpg'
